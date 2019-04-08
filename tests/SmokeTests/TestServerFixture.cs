78cff7eb60d8d8c0633d6c30488e9e3c8af4350f
srinivas.sunkari <srinivas.sunkari@zuto.com>
2019-04-08 22:40:26 +0100

    Refactor removed redundant configuration setting :alembic:

--- tests/SmokeTests/TestServerFixture.cs
+++ tests/SmokeTests/TestServerFixture.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.IO;
 using System.Net.Http;
 using Api;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.PlatformAbstractions;
 
 namespace SmokeTests
 {
@@ -16,20 +14,12 @@ namespace SmokeTests
         public TestServerFixture()
         {
             var builder = new WebHostBuilder()
-                .UseContentRoot(GetContentRootPath())
                 .UseEnvironment("Development")
                 .UseStartup<Startup>();  // Uses Start up class from your API Host project to configure the test server
 
             _testServer = new TestServer(builder);
             Client = _testServer.CreateClient();
 
-        }
-
-        private string GetContentRootPath()
-        {
-            var testProjectPath = PlatformServices.Default.Application.ApplicationBasePath;
-            var relativePathToHostProject = @"..\..\..\..\..\src\Api";
-            return Path.Combine(testProjectPath, relativePathToHostProject);
         }
 
         public void Dispose()
