57f6ae9750dca8f9ee11c2a1fe3e78ee983d8ca7
srinivas.sunkari <srinivas.sunkari@zuto.com>
2019-04-08 21:34:09 +0100

    Added default .net core web api project to the solution.

---
+++ src/Api/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetHealth(int id)
+        {
+            return "Healthy";
+        }
+    }
+}
