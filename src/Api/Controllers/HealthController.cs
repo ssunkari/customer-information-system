0f4331ff75945a7e56f265828aa2ad7574ad2751
srinivas.sunkari <srinivas.sunkari@zuto.com>
2019-04-08 22:12:57 +0100

    Added smoke tests to the application to verify health endpoint :tada:

--- src/Api/Controllers/HealthController.cs
+++ src/Api/Controllers/HealthController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
@@ -10,10 +11,22 @@ namespace Api.Controllers
     [ApiController]
     public class HealthController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<string> GetHealth(int id)
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HealthController(IHostingEnvironment hostingEnvironment)
         {
-            return "Healthy";
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet]
+        public ActionResult GetHealth(int id)
+        {
+            var status = new
+            {
+                Environment = _hostingEnvironment.EnvironmentName
+            };
+
+            return Ok(status);
         }
     }
 }
---
+++ tests/SmokeTests/HealthControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SmokeTests
+{
+
+    public class HealthControllerTests
+    {
+        private TestServerFixture _fixture;
+
+        [OneTimeSetUp]
+        public void BeforeTest()
+        {
+            _fixture = new TestServerFixture();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _fixture.Dispose();
+        }
+
+        [Test]
+        public async Task HealthControllerHappyPath()
+        {
+            var response = await _fixture.Client.GetAsync("api/health");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseStrong = await response.Content.ReadAsStringAsync();
+
+            responseStrong.Should().Be("{\"environment\":\"Development\"}");
+        }
+    }
+}
