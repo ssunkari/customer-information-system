e30191a2aa5ce03b55ad07202029bfe4513d863d
srinivas.sunkari <srinivas.sunkari@zuto.com>
2019-04-11 00:05:22 +0100

    created the couchbase index for querying N1Q1 queries :zap:

--- couchbase/configure.sh
+++ couchbase/configure.sh
@@ -31,24 +31,30 @@ done
 
 # Setup index and memory quota
 log "$(date +"%T") Init cluster ........."
-couchbase-cli cluster-init -c 127.0.0.1 --cluster-username Administrator --cluster-password password \
-  --cluster-name cis-cluster --cluster-ramsize 256 --cluster-index-ramsize 256 --services data,index,query,fts \
+couchbase-cli cluster-init -c 127.0.0.1 --cluster-username $COUCHBASE_ADMINISTRATOR_USERNAME --cluster-password $COUCHBASE_ADMINISTRATOR_PASSWORD \
+  --cluster-name couchbase-cluster --cluster-ramsize 256 --cluster-index-ramsize 256 --services data,index,query,fts \
   --index-storage-setting default
 
 # Create the buckets
 log "$(date +"%T") Create buckets ........."
-couchbase-cli bucket-create -c 127.0.0.1 --username Administrator --password password --bucket-type couchbase --bucket-ramsize 128 --bucket customer-bucket
+couchbase-cli bucket-create -c 127.0.0.1 --username $COUCHBASE_ADMINISTRATOR_USERNAME --password $COUCHBASE_ADMINISTRATOR_PASSWORD --bucket-type couchbase --bucket-ramsize 128 --bucket  $COUCHBASE_BUCKET
 
 # Create user
 log "$(date +"%T") Create users ........."
-couchbase-cli user-manage -c 127.0.0.1:8091 -u Administrator -p password --set --rbac-username sysadmin --rbac-password password \
+couchbase-cli user-manage -c 127.0.0.1:8091 -u $COUCHBASE_ADMINISTRATOR_USERNAME -p $COUCHBASE_ADMINISTRATOR_PASSWORD --set --rbac-username sysadmin --rbac-password password \
  --rbac-name "sysadmin" --roles admin --auth-domain local
 
-couchbase-cli user-manage -c 127.0.0.1:8091 -u Administrator -p password --set --rbac-username admin --rbac-password password \
+couchbase-cli user-manage -c 127.0.0.1:8091 -u $COUCHBASE_ADMINISTRATOR_USERNAME -p $COUCHBASE_ADMINISTRATOR_PASSWORD --set --rbac-username admin --rbac-password password \
  --rbac-name "admin" --roles bucket_full_access[*] --auth-domain local
 
 # Need to wait until query service is ready to process N1QL queries
 log "$(date +"%T") Waiting ........."
 sleep 10
+
+# Create bucket1 indexes
+echo "$(date +"%T") Create bucket1 indexes ........."
+cbq -u Administrator -p password -s "CREATE PRIMARY INDEX idx_primary ON \`$COUCHBASE_BUCKET\`;"
+cbq -u Administrator -p password -s "CREATE INDEX idx_type ON \`$COUCHBASE_BUCKET\`(_type);"
+cbq -u Administrator -p password -s "CREATE INDEX idx_account_id ON \`$COUCHBASE_BUCKET\`(id) WHERE (_type = \"account\");"
 
 fg 1
